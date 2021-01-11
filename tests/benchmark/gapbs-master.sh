1ef5f89e9f2e88473ab199e3c59b70e170f6ebb4
John Leidel <john.leidel@gmail.com>
2021-01-11 06:40:01 -0600

    enabling parallel openmp builds for gapbs

--- tests/benchmark/gapbs-master.sh
+++ tests/benchmark/gapbs-master.sh
@@ -33,7 +33,7 @@ cd $SRC
 #------------------------------------------------
 # STAGE-3: INITIATE THE BUILD
 #------------------------------------------------
-export SERIAL=1
+#export SERIAL=1
 CXX=$RV_CXX CXX_FLAGS="$RV_CXXFLAGS" make -j$MAX_THREADS
 
 exit 0
