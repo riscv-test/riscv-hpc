98c9e58bb1883424544e555d0ee631674e1e9588
John Leidel <john.leidel@gmail.com>
2021-01-18 15:36:49 -0600

    updating openmp build

--- tests/compilers/llvm-project-master.sh
+++ tests/compilers/llvm-project-master.sh
@@ -42,7 +42,7 @@ make install
 
 #-- remove the current build so we can reconstruct everything for RISC-V
 rm -Rf ./*
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_C_COMPILER=$INSTALL_PATH/bin/clang -DCMAKE_CXX_COMPILER=$INSTALL_PATH/bin/clang++ -DCMAKE_C_FLAGS="$RV_CFLAGS" -DCMAKE_CXX_FLAGS="$RV_CXXFLAGS" -DLIBOMP_ARCH=riscv64 ../openmp
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_C_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-gcc -DCMAKE_CXX_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-g++ -DLIBOMP_ARCH=riscv64 ../openmp
 make -j$MAX_THREADS
 make install
 
