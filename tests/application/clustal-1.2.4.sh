1647925f7a40cab3845c59c12058cf1e5d4cb21e
John Leidel <john.leidel@gmail.com>
2021-03-08 09:26:27 -0600

    adding clustal application test

---
+++ tests/application/clustal-1.2.4.sh
@@ -0,0 +1,53 @@
+#!/bin/bash -xe
+#
+# RISC-V HPC Test Suite
+#
+# /application/clustal-1.2.4.sh
+#
+# Clustal 1.2.4
+#
+# http://www.clustal.org/omega/
+#
+
+#------------------------------------------------
+# TOP-LEVEL VARIABLES
+#------------------------------------------------
+TEST=$(basename $0)
+REPO=
+ARCHIVE=http://www.clustal.org/omega/clustal-omega-1.2.4.tar.gz
+#------------------------------------------------
+
+#------------------------------------------------
+# STAGE-1: SOURCE THE ENVIRONMENT SCRIPT
+#------------------------------------------------
+. $WORKSPACE/common/build_env.sh $TEST $NODE_NAME "$NODE_LABELS" $MAX_THREADS $REPO $ARCHIVE
+
+#------------------------------------------------
+# STAGE-2: CLONE THE REPO
+#------------------------------------------------
+cd $BUILDROOT
+rm -Rf $SRC
+git clone $REPO $SRC
+cd $SRC
+
+#------------------------------------------------
+# STAGE-3: INITIATE THE BUILD
+#------------------------------------------------
+CC="$RV_CC" CFLAGS="$RV_CFLAGS" CXX="$RV_CXX" CXXFLAGS="$RV_CXXFLAGS" ./configure --prefix=$INSTALL_PATH
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+make
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+make install
+if [ $? -ne 0 ]; then
+  exit -1
+fi
+
+exit 0
+
+# EOF
