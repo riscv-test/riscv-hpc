a1a19eeb19642e97a5d515d275c60dd762cdd0a0
John Leidel <john.leidel@gmail.com>
2021-07-27 07:13:21 -0500

    disabling libomptarget for RISC-V builds

--- tests/compilers/llvm-project-master.sh
+++ tests/compilers/llvm-project-master.sh
@@ -49,9 +49,9 @@ if [ $? -ne 0 ]; then
   exit -1
 fi
 
-#-- remove the current build so we can reconstruct everything for RISC-V
+#-- remove the current build so we can reconstruct everything for RISC-V openmp
 rm -Rf ./*
-cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_C_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-gcc -DCMAKE_CXX_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-g++ -DLIBOMP_ARCH=riscv64 ../openmp
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH -DCMAKE_C_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-gcc -DCMAKE_CXX_COMPILER=$RV_SYSROOT/bin/riscv64-unknown-linux-gnu-g++ -DLIBOMP_ARCH=riscv64 -DOPENMP_ENABLE_LIBOMPTARGET=OFF ../openmp
 if [ $? -ne 0 ]; then
   exit -1
 fi
