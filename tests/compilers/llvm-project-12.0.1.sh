1d9783806c3edfb80183e8bda22ea9b89b8bfbbf
John Leidel <john.leidel@gmail.com>
2021-07-27 13:46:46 -0500

    adding build scripts for llvm 12.0.0 and 12.0.1

---
+++ tests/compilers/llvm-project-12.0.1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -xe
+#
+# RISC-V HPC Test Suite
+#
+# llvm-project-12.0.1.sh
+#
+# RISC-V LLVM Toolchain: 12.0.1 Release
+#
+# https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.1.tar.gz
+#
+
+#------------------------------------------------
+# TOP-LEVEL VARIABLES
+#------------------------------------------------
+TEST=$(basename $0)
+REPO=
+ARCHIVE=https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-12.0.1.tar.gz
+#------------------------------------------------
+
+
+#------------------------------------------------
+# STAGE-1: SOURCE THE ENVIRONMENT SCRIPT
+#------------------------------------------------
+. $WORKSPACE/common/build_env.sh $TEST $NODE_NAME "$NODE_LABELS" $MAX_THREADS $REPO $ARCHIVE
+
+#------------------------------------------------
+# STAGE-2: CLONE THE REPO
+#------------------------------------------------
+#-- IGNORED: PREBUILT LIBRARY
+
+#------------------------------------------------
+# STAGE-3: INITIATE THE BUILD
+#------------------------------------------------
+# Check to ensure that the compiler exists
+if [ -s "$RV_CC" ];
+then
+  echo "Preinstalled compiler exists at $RV_CC"
+else
+  echo "Preinstalled compiler NOT FOUND at $RV_CC"
+  exit -1
+fi
+
+exit 0
+
+# EOF
